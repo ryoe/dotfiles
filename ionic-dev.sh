bd20aa1c5ae7aefb94a6fd6d24150c959661ed34
Rick Yoesting <rick.yoesting@gmail.com>
2015-10-27 16:45:09 -0400

    don't use brew for node. add n for node version management

--- ionic-dev.sh
+++ ionic-dev.sh
@@ -1,14 +1,21 @@
 # Check for node 
 if test ! $(which node); then
-  echo "Installing node..."
-  brew install node
+  echo "You must install node..."
 fi
 
-# gulp and bower because that's what you do!
-npm install -g gulp bower
+if test $(which node); then
+  # gulp and bower because that's what you do!
+  npm install -g gulp bower
 
-# cordova and ionic are must haves
-npm install -g cordova ionic
+  # cordova and ionic are must haves
+  npm install -g cordova ionic
 
-# karma is our test runner
-npm install -g karma-cli
+  # karma is our test runner
+  npm install -g karma-cli
+
+  # n - for managing node versions
+  npm install -g n
+
+  # install node v0.12.7
+  n 0.12.7
+fi
