1622c6464fcabc7a74742da6b0da79f13aaa5ca4
Rick Yoesting <rick.yoesting@gmail.com>
2015-10-28 11:14:03 -0400

    add java_home symlink if you need to

---
+++ set_java_home.sh
@@ -0,0 +1,4 @@
+# after java installed
+# so you can set JAVA_HOME like so:
+# export JAVA_HOME=$(/usr/libexec/java_home)
+sudo ln -s /System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home /usr/libexec/java_home
