59692828c3b412db6807dd0c2c5f472b53e9f4f4
Rick Yoesting <rick.yoesting@gmail.com>
2015-10-28 10:01:18 -0400

    sudo stuff...

--- ionic-dev.sh
+++ ionic-dev.sh
@@ -5,16 +5,16 @@ fi
 
 if test $(which node); then
   # gulp and bower because that's what you do!
-  npm install -g gulp bower
+  sudo npm install -g gulp bower
 
   # cordova and ionic are must haves
-  npm install -g cordova ionic
+  sudo npm install -g cordova ionic
 
   # karma is our test runner
-  npm install -g karma-cli
+  sudo npm install -g karma-cli
 
   # n - for managing node versions
-  npm install -g n
+  sudo npm install -g n
 
   # install node v0.12.7
   n 0.12.7
