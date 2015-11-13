593610cdcd022634ff903b238ff93ef7c3107445
Rick Yoesting <rick.yoesting@gmail.com>
2015-11-13 16:23:08 -0500

    install fastlane tools

---
+++ install-fastlane-tools.sh
@@ -0,0 +1,25 @@
+# brew install ruby
+
+
+# fastlane - https://fastlane.tools/
+sudo gem install fastlane
+
+# ios tools
+sudo gem install cert
+sudo gem install sigh
+sudo gem install produce
+sudo gem install deliver
+sudo gem install gym
+sudo gem install pilot
+sudo gem install snapshot
+
+# others we're not using yet...
+# sudo gem install scan
+# sudo gem install pem
+
+# not likely to use
+# sudo gem install boarding
+# sudo gem install frameit
+
+# android tools...
+sudo gem install supply
